397403b037f2ccf27f0db08ed7a9149ae0972786
Ilyha-deriy <68822351+Ilyha-deriy@users.noreply.github.com>
2021-08-11 16:34:46 +0300

    Add files via upload

---
+++ config/constants.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+define('LOCALHOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_NAME', 'task_manager');
+
+define('SITEURL', 'http://localhost:8888/task-manager/');
+
+ ?>
